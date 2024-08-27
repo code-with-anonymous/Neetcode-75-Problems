91fa0babbeb9d50dad22635649f5ec4aff0890db
Muhammad Rayyan <146942220+code-with-anonymous@users.noreply.github.com>
2024-08-27 10:57:53 +0500

    Time: 48 ms (62.08%), Space: 16.9 MB (46.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # return sorted(s) == sorted(t)
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+
+        return True
+
+
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         # return sorted(s) == sorted(t)
+
+        # return Counter(s) == Counter(t)      
+
 # point to ponder that sort and and sorted are diffrent fuction        
 #                 Key Differences:
 #  Modification of Original Data:
