fa4da1e391b6bf39ec53b95ea39ec6d16fa06edc
Muhammad Rayyan <146942220+code-with-anonymous@users.noreply.github.com>
2024-08-27 10:48:45 +0500

    Time: 43 ms (83.67%), Space: 17 MB (46.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
-        
+        # return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
+# point to ponder that sort and and sorted are diffrent fuction        
+#                 Key Differences:
+#  Modification of Original Data:
+
+# sort(): Modifies the list in place.
+# sorted(): Does not modify the original iterable; returns a new sorted list.
+# Return Value:
+
+# sort(): Returns None.
+# sorted(): Returns a new sorted list.
+# Applicability:
+
+# sort(): Only works on lists.
+# sorted(): Can work on any iterable (e.g., lists, tuples, strings).
