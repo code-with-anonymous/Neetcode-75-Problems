376e77b217be75ca982cd47d67aa6303b29c63d2
Muhammad Rayyan <146942220+code-with-anonymous@users.noreply.github.com>
2024-08-28 10:47:14 +0500

    Time: 62 ms (53.26%), Space: 17.8 MB (10.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,30 +1,43 @@
-from typing import List
+# from typing import List
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         # Initialize pointers and index list
+#         index = []
+#         left = 0
+#         right = len(nums) - 1
+        
+#         # Sort the list to use two-pointer approach
+#         nums_with_index = sorted((num, i) for i, num in enumerate(nums))
+        
+#         # Reinitialize pointers based on sorted list
+#         left = 0
+#         right = len(nums_with_index) - 1
+
+#         while left < right:
+#             current_sum = nums_with_index[left][0] + nums_with_index[right][0]
+            
+#             if current_sum == target:
+#                 index.append(nums_with_index[left][1])
+#                 index.append(nums_with_index[right][1])
+#                 return index  # Return as soon as we find the solution
+            
+#             elif current_sum < target:
+#                 left += 1  # Increase sum by moving left pointer right
+#             else:
+#                 right -= 1  # Decrease sum by moving right pointer left
+
+#         return index  # Return empty list if no pair found (though usually a solution exists)
+
+
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Initialize pointers and index list
-        index = []
-        left = 0
-        right = len(nums) - 1
+        prevMap = {}  # val : index
         
-        # Sort the list to use two-pointer approach
-        nums_with_index = sorted((num, i) for i, num in enumerate(nums))
-        
-        # Reinitialize pointers based on sorted list
-        left = 0
-        right = len(nums_with_index) - 1
-
-        while left < right:
-            current_sum = nums_with_index[left][0] + nums_with_index[right][0]
-            
-            if current_sum == target:
-                index.append(nums_with_index[left][1])
-                index.append(nums_with_index[right][1])
-                return index  # Return as soon as we find the solution
-            
-            elif current_sum < target:
-                left += 1  # Increase sum by moving left pointer right
-            else:
-                right -= 1  # Decrease sum by moving right pointer left
-
-        return index  # Return empty list if no pair found (though usually a solution exists)
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
