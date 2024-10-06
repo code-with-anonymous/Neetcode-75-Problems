70f05f0c8ecf7da6ef142d0159ef68b28d56eb1e
Muhammad Rayyan <146942220+code-with-anonymous@users.noreply.github.com>
2024-10-06 22:57:58 +0500

    Time: 185 ms (85.97%), Space: 18.2 MB (33.35%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,28 +1,38 @@
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         start =0
+#         end =len(nums)-1
+#         if len(nums) == 1 and nums[start] != target:
+#                 return -1 
+#         elif len(nums) == 1 and nums[start] == target:
+#             return 0        
+#         while start <= end:
+#             if nums [start] == target:
+#                 return start
+#             if nums [end] == target:
+#                 return end
+
+#             if nums[start] < target :
+#                 start+=1
+#             if nums[end] > target :
+
+#                 end-=1
+#         return -1 
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start =0
-        end =len(nums)-1
-        if len(nums) == 1 and nums[start] != target:
-                return -1 
-        elif len(nums) == 1 and nums[start] == target:
-            return 0        
+        start = 0
+        end = len(nums) - 1
+        
         while start <= end:
-            if nums [start] == target:
-                return start
-            if nums [end] == target:
-                return end
-
-            if nums[start] < target :
-                start+=1
-            if nums[end] > target :
-
-                end-=1
-        return -1 
-
-
-
-
-
-
-            # if len(nums) == 1 and nums[start] != target:
-            #     return -1
+            mid = start + (end - start) // 2  # Calculate middle element
+            
+            if nums[mid] == target:
+                return mid  # Target found at mid
+            elif nums[mid] < target:
+                start = mid + 1  # Search in the right half
+            else:
+                end = mid - 1  # Search in the left half
+                
+        return -1  # Target not found
